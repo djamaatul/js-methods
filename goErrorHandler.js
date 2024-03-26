8c389a9e286ad67f12bb8d46e3ae61612186f9d6
D. Jama'atul Anbiya <70568486+djamaatul@users.noreply.github.com>
2024-03-26 09:36:24 +0700

    Update goErrorHandler.js

--- goErrorHandler.js
+++ goErrorHandler.js
@@ -1,8 +1,12 @@
-const safe = async (fn) => {
-	try {
-		const data = await fn();
-		return [null, data];
-	} catch (error) {
-		return [error, null];
-	}
+/**
+ * @type {(fn: function, onError?: function)=> Promise<[Error|null,any|null]>}
+ */
+const safe = async (fn, onError) => {
+    try {
+        const data = await fn();
+        return [null, data];
+    } catch (error) {
+        await onError?.(error);
+        return [error, null];
+    }
 };
